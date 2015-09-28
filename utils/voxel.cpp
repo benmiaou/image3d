72bb61cab1110226cd6913cfbb18b1ac01162104
benmiaou <benmiaou@hotmail.com>
2015-09-28 09:41:29 +0200

    filter feces

--- utils/voxel.cpp
+++ utils/voxel.cpp
@@ -11,42 +11,54 @@ voxel::voxel(pgm3D pgm)
     for(int x = 0; x < width; x++)
         for(int y = 0; y<height; y++)
             for(int z =0; z<depth; z++)
-              if(values[x*depth*height+y*depth+z]!=0){
-                  int grey = values[x*depth*height+y*depth+z];
-                  point a(x-0.5,y-0.5,z-0.5);
-                  point b(x-0.5,y+0.5,z-0.5);
-                  point c(x+0.5,y+0.5,z-0.5);
-                  point d(x+0.5,y-0.5,z-0.5);
-                  point e(x-0.5,y-0.5,z+0.5);
-                  point f(x-0.5,y+0.5,z+0.5);
-                  point g(x+0.5,y+0.5,z+0.5);
-                  point h(x+0.5,y-0.5,z+0.5);
+                if(values[x*depth*height+y*depth+z]!=0){
+                    int grey = values[x*depth*height+y*depth+z];
+                    point a(x-0.5,y-0.5,z-0.5);
+                    point b(x-0.5,y+0.5,z-0.5);
+                    point c(x+0.5,y+0.5,z-0.5);
+                    point d(x+0.5,y-0.5,z-0.5);
+                    point e(x-0.5,y-0.5,z+0.5);
+                    point f(x-0.5,y+0.5,z+0.5);
+                    point g(x+0.5,y+0.5,z+0.5);
+                    point h(x+0.5,y-0.5,z+0.5);
+                    quad q(a,b,c,d);
 
-                  quad q(a,b,c,d);
-                  q.grey = grey;
-                  quads.push_back(q);
+                    if(values[x*depth*height+y*depth+z-1]==0){//z-1
+                        q = quad(a,b,c,d);
+                        q.grey = grey;
+                        quads.push_back(q);
+                    }
 
-                  q = quad(a,b,f,e);
-                  q.grey = grey;
-                  quads.push_back(q);
+                    if(values[(x-1)*depth*height+y*depth+z]==0){//x-1
+                        q = quad(a,b,f,e);
+                        q.grey = grey;
+                        quads.push_back(q);
+                    }
 
-                  q= quad(e,f,g,h);
-                  q.grey = grey;
-                  quads.push_back(q);
+                    if(values[x*depth*height+y*depth+z+1]==0){//z+1
+                        q= quad(e,f,g,h);
+                        q.grey = grey;
+                        quads.push_back(q);
+                    }
 
-                  q= quad(h,g,c,d);
-                  q.grey = grey;
-                  quads.push_back(q);
+                    if(values[(x+1)*depth*height+y*depth+z]==0){//x+1
+                        q= quad(h,g,c,d);
+                        q.grey = grey;
+                        quads.push_back(q);
+                    }
 
-                  q= quad(a,d,h,e);
-                  q.grey = grey;
-                  quads.push_back(q);
+                    if(values[x*depth*height+(y-1)*depth+z]==0){//y-1
+                        q= quad(a,d,h,e);
+                        q.grey = grey;
+                        quads.push_back(q);
+                   }
+                    if(values[x*depth*height+(y+1)*depth+z]==0){//y+1
+                        q= quad(b,c,g,f);
+                        q.grey = grey;
+                        quads.push_back(q);
+                    }
 
-                  q= quad(b,c,g,f);
-                  q.grey = grey;
-                  quads.push_back(q);
-
-              }
+                }
 
 
 }
